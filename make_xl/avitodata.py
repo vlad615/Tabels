3ce00d67f8395d7927794e3edc6bea28b384cd07
Vlad FormWork <ambrosov413@gmail.com>
2025-04-24 11:54:15 +0300

    обновление таблиц

--- make_xl/avitodata.py
+++ make_xl/avitodata.py
@@ -10,11 +10,10 @@ def set_id(count_id: int) -> list[int]:
 
 def set_address(cont_id: int) -> tuple[list[str], str]:
     addresses = ["Москва, Беловежская улица", "Москва, Часовая улица", "Москва, 7-я Парковая улица",
-                 "Москва, Чертановская улица", "Московская область, Одинцово", "Московская область, Химки",
-                 "Московская область, Подольк", "Московская область, Балашиха"]
+                 "Москва, Чертановская улица", "Московская область, Одинцово"]
 
-    if cont_id // 8:
-        addresses = addresses * (cont_id // 8 + 1)
+    if cont_id // 5:
+        addresses = addresses * (cont_id // 5 + 1)
     shuffle(addresses)
 
     filepath = filedialog.asksaveasfilename(filetypes=(('Excel files', '*.xlsx'), ), initialfile="avito_table")
@@ -24,10 +23,10 @@ def set_address(cont_id: int) -> tuple[list[str], str]:
 if __name__ == "__main__":
     assert len(set_id(15)) == 15
     assert len(set_address(1)) == 1
-    assert set_address(15) == 15
-    assert set_address(40) == 40
-    assert set_address(25) == 25
-    assert set_address(17) == 17
-    assert set_address(10) == 10
-    assert set_address(8) == 8
-    assert set_address(5) == 5
+    assert len(set_address(15)) == 15
+    assert len(set_address(40)) == 40
+    assert len(set_address(25)) == 25
+    assert len(set_address(17)) == 17
+    assert len(set_address(10)) == 10
+    assert len(set_address(8)) == 8
+    assert len(set_address(5)) == 5
